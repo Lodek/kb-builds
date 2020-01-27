c31a5ce910bd7fb16e10741f0ab0d94f38739293
Lodek <23224552+Lodek@users.noreply.github.com>
2020-01-26 23:32:50 -0300

    Build files for gamma keyboard

---
+++ gamma/defines.h
@@ -0,0 +1,14 @@
+#define INPUTS_LEN 10
+#define OUTPUTS_LEN 8
+
+#define HOLD_DELAY 500
+#define RETAP_DELAY 500
+
+//csv notation
+#define OUTPUT_PINS A0,A1,A2,A3,0,1,14,15
+#define INPUT_PINS  10,6,7,8,9,2,3,4,16,5
+
+#define OUTPUT_OFF HIGH
+#define OUTPUT_ON LOW
+//1 enables pullups 0 disables it
+#define INPUT_PULLUP 1
